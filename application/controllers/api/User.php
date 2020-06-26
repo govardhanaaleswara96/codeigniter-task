9655a8cfd99e7530ca18e2d7640d937d8d7dc146
govardhanaaleswara96 <govardhanaaleswara88@gmail.com>
2020-06-26 19:16:53 +0530

    first commit

---
+++ application/controllers/api/User.php
@@ -0,0 +1,65 @@
+<?php
+class User extends CI_Controller 
+{
+	public function __construct()
+	{
+	parent::__construct();
+	$this->load->database();
+	$this->load->model('User_Model');
+	}
+	// create user 
+	
+	public function createUser()
+	{	 
+	if(!empty($this->input->post('userName'))
+	&& !empty($this->input->post('userMail'))
+	&& !empty($this->input->post('userPass'))
+	&& !empty($this->input->post('userLevel')))
+	{
+	$userName=$this->input->post('userName');
+	$userMail=$this->input->post('userMail');
+	$userPass=md5($this->input->post('userPass'));
+	$userLevel=$this->input->post('userLevel');
+	$data = array(
+	'userName' => $userName,
+	'userMail' => $userMail,
+	'userPass' =>$userPass,
+	'userLevel' =>$userLevel
+	); 
+	//load user model
+	$this->User_Model->insertUser($data);
+	$userJson['success'] = 'User Created Successfully';
+	$userJson['error'] = '';
+	}
+	else {
+		$userJson['success'] = '';
+		$userJson['error'] = 'User Creation Failed';
+
+	}
+	$this->output->set_output(json_encode($userJson));	
+	}
+
+	// user login 
+
+    public function logIn(){
+	if(!empty($this->input->post('userMail'))
+	&& !empty($this->input->post('userPass')))
+	{
+	$userMail = $this->input->post('userMail');
+	$userPass = $this->input->post('userPass');
+	//load user model
+	$result = $this->User_Model->loginUser($userMail,$userPass);
+	$userJson['message'] = $result;
+	$this->output->set_output(json_encode($userJson));
+	}
+	else {
+		$userJson['message'] = $result;
+
+	$userJson['success'] = '';
+	$userJson['error'] = 'please Enter Valid Details';
+	$this->output->set_output(json_encode($userJson));	
+
+	}
+	}	
+}
+?>
---
+++ application/models/User_Model.php
@@ -0,0 +1,39 @@
+<?php
+class User_Model extends CI_Model 
+{
+	// create user 
+      
+	function insertUser($data)
+	{
+    $this->db->insert('users', $data);
+	}
+	// user login 
+
+	function loginUser($userMail,$userPass)
+	{
+	$this->db->where('userMail', $userMail);
+	$query = $this->db->get('users');
+	if($query->num_rows() > 0)
+	{
+	foreach($query->result() as $row)
+	{
+	$store_password = $row->userPass;
+	// password compare
+	if(md5($userPass) == $store_password)
+	{
+	return 'User Login Successfully';
+	// set user id for session 
+	$this->session->set_userdata('id', $row->id);
+	}
+	else
+	{
+	return 'Password Is Incorrect';
+	}		
+	}
+	}
+	else
+	{
+	return 'Invalid User Credentials';
+	}
+	}
+}
